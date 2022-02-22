3724e7e01a8c492f5e07405104561c5c76071352
Oleg <VasilchenkoOV1988@gmail.com>
2022-02-22 13:51:22 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+"use sctrict";
+
+
+let incr = 10,
+    decr = 10;
+
+
+    console.log(++incr);
+    console.log(--decr);
+
+    console.log(5%2);
+
+    console.log(2+2*2 != 8);
+
+    const isChecked = false,
+        isClose = false;
+
+        console.log(isChecked || !isClose);
