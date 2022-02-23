d5529365c9c0d50f01f04b97e850e9c8c58004f4
Oleg <VasilchenkoOV1988@gmail.com>
2022-02-23 20:29:10 +0200

    Методы и свойства строк и чисел

--- js/script.js
+++ js/script.js
@@ -1,43 +1,81 @@
 "use strict";
 
+const str = "test";
 
-//! Function Declaration
-let num = 20;
+// console.log(str[2]);
 
-function showFirstMessage(text) {
-    console.log(text);
-    let num = 10;
-    console.log(num);
-}
+console.log(str.toUpperCase()); //? Преобразовует строку в верхний регистр
 
-showFirstMessage('я программист');
-console.log(num);
+console.log(str.toLowerCase()); //? Преобразовует строку в нижний регистр
 
-function calc(a, b) {
-    return (a + b);
-}
-console.log(calc(4, 3));
+let fruit = "Some fruit";
+console.log(fruit.indexOf('q')); //? Возвращает индекс символа строки
 
 
-function ret() {
-    let num = 50;
-    return num;
-}
-const anotherNum = ret();
-console.log(anotherNum);
+
+console.log(logg.slice(6, 11)); //? Возвращает символы строки с начального по конечный индекс, которые указыватся в скобках
 
 
-//! Function Expression
+console.log(logg.substring(6, 11)); //? Возвращает символы строки с начального по конечный индекс, которые указыватся в скобках
 
-const logger = function() {
-    console.log("Hello World!");
-};
-logger();
+const logg = 'Hello World';
 
-//! Стрелочная функция
-const calct = (a, b) => a + b; //? Если тело функции в одну строку, то можно не ставить фигурные скобки {}
+console.log(logg.substr(6, 5)); //? Возвращает символы строки с начального индекса по количнству указанных символов (второй аргумент), которые указыватся в скобках
 
-const calcte = (a, b) => {
-console.log('1');
-return a + b;
-};
+
+//! Методы чисел
+
+const num = 12.2;
+
+console.log(Math.round(num)); //? Метод округления чисел
+
+const test = '12.2px';
+// console.log(parseInt(test));
+console.log(parseFloat(test));
+
+
+
+
+
+
+
+// //! Function Declaration
+// let num = 20;
+
+// function showFirstMessage(text) {
+//     console.log(text);
+//     let num = 10;
+//     console.log(num);
+// }
+
+// showFirstMessage('я программист');
+// console.log(num);
+
+// function calc(a, b) {
+//     return (a + b);
+// }
+// console.log(calc(4, 3));
+
+
+// function ret() {
+//     let num = 50;
+//     return num;
+// }
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+
+// //! Function Expression
+
+// const logger = function() {
+//     console.log("Hello World!");
+// };
+// logger();
+
+// //! Стрелочная функция
+// const calct = (a, b) => a + b; //? Если тело функции в одну строку, то можно не ставить фигурные скобки {}
+
+// const calcte = (a, b) => {
+// console.log('1');
+// return a + b;
+// };
