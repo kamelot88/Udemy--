14fdfee0b024df76c89e4068f8273644cd6a0f66
Oleg <VasilchenkoOV1988@gmail.com>
2022-02-23 16:16:25 +0200

    Функции

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-Udemy курс
+# Udemy курс
+
+## Список необходимых плагинов:
+1. All Autocomplete
+2. Auto Close Tag
+3. Auto Complete Tag
+4. Auto Rename Tag
+5. Beautify
+6. Code Runner
+7. Import Cost
+8. JavaScript (ES6) code snippets
+9. jshint
+10. Live Server
+11. Multiple clipboards for VSCode
+12. Reactjs code snippets
+13. Sass
+14. Theme - Oceanic Next
+15. Theme - Identical Sublime Text Monokai theme
+16. vscode-icons
+17. ESLint
+
--- js/script.js
+++ js/script.js
@@ -1,89 +1,37 @@
 "use strict";
 
-const personalMovieDB = {
-    count: '',
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
-};
 
-//! Цикл for
-// personalMovieDB.count = prompt('Сколько фильмов вы уже посмотрели?', '');
-// for (let i = 0; i < 2; i++) {
-//         const a = prompt('Один из последних просмотренных фильмов?', ''),
-//               b = prompt('На сколько оцените его?');
-//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-//         personalMovieDB.movies[a] = b;
-//         console.log('done');
-//     } else {
-//         console.log('Error');
-//         i--;
-//     }
-// }
+//! Function Declaration
+let num = 20;
 
-//! Цикл while
-// personalMovieDB.count = prompt('Сколько фильмов вы уже посмотрели?', '');
-// let i = 0;
-// while (i < 2) {
-//         const a = prompt('Один из последних просмотренных фильмов?', ''),
-//               b = prompt('На сколько оцените его?');
-//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-//         personalMovieDB.movies[a] = b;
-//         console.log('done');
-//     } else {
-//         console.log('Error');
-//         i--;
-//     }
-//     i++;
-// }
-
-//! Цикл do…while
-personalMovieDB.count = prompt('Сколько фильмов вы уже посмотрели?', '');
-let i = 0;
-do {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-                  b = prompt('На сколько оцените его?');
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else {
-            console.log('Error');
-            i--;
-        }
-        i++;
-} while (i < 2);
-
-
-
-
-if (personalMovieDB.count < 10) {
-    console.log('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log('Вы класический зритель');
-} else if (personalMovieDB.count >= 30) {
-    console.log('Вы киноман');
-} else {
-    console.log('Произошла ошибка');
+function showFirstMessage(text) {
+    console.log(text);
+    let num = 10;
+    console.log(num);
 }
-console.log(personalMovieDB);
+
+showFirstMessage('я программист');
+console.log(num);
+
+function calc(a, b) {
+    return (a + b);
+}
+console.log(calc(4, 3));
 
 
+function ret() {
+    let num = 50;
+    return num;
+}
+const anotherNum = ret();
+console.log(anotherNum);
 
 
-// let number = 50;
-// for (let i = 1; i < 10; i++) {
-//     if (i === 6) {
-//         break; //? остановит выполнение цикла при достижении переменной i значения "6"
-//     }
-//     console.log(i);
-// }
+//! Function Expression
 
+const logger = function() {
+    console.log("Hello World!");
+};
+logger();
 
-// let lit = 50;
-// for (let i = 1; i < 10; i++) {
-//     if (i === 6) {
-//         continue; //? пропустит выполнение кода при достижении переменной i значения "6", после чего продолжит выполнение кода со следующего цикла
-//     }
-//     console.log(i);
-// }
+//! Стрелочная функция
