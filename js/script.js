48fb8bf70f2540eec1d1befb3c60a75e89fed971
Oleg <VasilchenkoOV1988@gmail.com>
2022-02-23 15:15:42 +0200

    Применение условий и циклов

--- js/script.js
+++ js/script.js
@@ -1,39 +1,89 @@
 "use strict";
 
-//! Циклы
+const personalMovieDB = {
+    count: '',
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
 
-let num = 50;
+//! Цикл for
+// personalMovieDB.count = prompt('Сколько фильмов вы уже посмотрели?', '');
+// for (let i = 0; i < 2; i++) {
+//         const a = prompt('Один из последних просмотренных фильмов?', ''),
+//               b = prompt('На сколько оцените его?');
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('Error');
+//         i--;
+//     }
+// }
 
-while (num <= 55) {
-    console.log(num);
-    num++;
-}
+//! Цикл while
+// personalMovieDB.count = prompt('Сколько фильмов вы уже посмотрели?', '');
+// let i = 0;
+// while (i < 2) {
+//         const a = prompt('Один из последних просмотренных фильмов?', ''),
+//               b = prompt('На сколько оцените его?');
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('Error');
+//         i--;
+//     }
+//     i++;
+// }
 
-
-
-
-let numb = 50;
+//! Цикл do…while
+personalMovieDB.count = prompt('Сколько фильмов вы уже посмотрели?', '');
+let i = 0;
 do {
-    console.log(numb);
-    numb++;
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+                  b = prompt('На сколько оцените его?');
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('Error');
+            i--;
+        }
+        i++;
+} while (i < 2);
+
+
+
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы класический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
 }
-while (numb < 55);
+console.log(personalMovieDB);
 
 
 
-let number = 50;
-for (let i = 1; i < 10; i++) {
-    if (i === 6) {
-        break; //? остановит выполнение цикла при достижении переменной i значения "6"
-    }
-    console.log(i);
-}
+
+// let number = 50;
+// for (let i = 1; i < 10; i++) {
+//     if (i === 6) {
+//         break; //? остановит выполнение цикла при достижении переменной i значения "6"
+//     }
+//     console.log(i);
+// }
 
 
-let lit = 50;
-for (let i = 1; i < 10; i++) {
-    if (i === 6) {
-        continue; //? пропустит выполнение кода при достижении переменной i значения "6", после чего продолжит выполнение кода со следующего цикла
-    }
-    console.log(i);
-}
+// let lit = 50;
+// for (let i = 1; i < 10; i++) {
+//     if (i === 6) {
+//         continue; //? пропустит выполнение кода при достижении переменной i значения "6", после чего продолжит выполнение кода со следующего цикла
+//     }
+//     console.log(i);
+// }
